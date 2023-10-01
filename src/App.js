b33d2771cf42dbefeb20ab74df4c944a11d301e4
Chavarattil Shivprasad Premarajan <shivprasadcp02@gmail.com>
2023-10-01 17:32:32 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Category from './components/Category';
 
 function App() {
   const [results, setResults] = useState([]);
 
   React.useEffect(() => {
     fetch("http://localhost:3001/categories")
-    .then(response => response.json())
-    .then(data =>{
-      console.log(data);
-      setResults(data); 
-    })
-  }, [])
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setResults(data);
+      });
+  }, []);
+
+  const renderCategories = () => {
+    return (
+      <div>
+        {
+          results.map(c =>
+            <Category key={c.id} id={c.id} title={c.title} />
+          )
+        }
+      </div>
+    );
+  };
+
+
   return (
-    <div className="App">
-      {
-        results.map(d => (
-          <div key={d.id}>{d.title}</div>
-        ))
-      }
-    </div>
+    <>
+      <header>My Store</header>
+
+      <section>
+        <nav>
+          {renderCategories()}
+        </nav>
+        <article>
+          main area
+        </article>
+      </section>
+
+      <footer>
+        footer
+      </footer>
+    </>
   );
 }
 
 export default App;
+
+
+  
